e41d8a1025be3d23f2fd119865b513b30430fed4
geraltyen <girireddyanna@gmail.com>
2023-02-22 16:17:47 +0530

    added full footer

--- Frontend/cruiserental/src/App.js
+++ Frontend/cruiserental/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Footer from './Components/Footer';
 import AllRoutes from './Routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
       <AllRoutes/>
+      <Footer/>
     </div>
   );
 }
